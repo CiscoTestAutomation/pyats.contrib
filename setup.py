1243dfd685ad6c48d4fd68c6c34f8bfd79a7e1ae
Ben Astell <bastell@cisco.com>
2020-12-15 16:29:10 -0500

    v20.12

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def discover_creators():
 # launch setup
 setup(
     name = 'pyats.contrib',
-    version = '20.10.3',
+    version = '20.12',
 
     # descriptions
     description = 'Open source package for pyATS framework extensions.',
