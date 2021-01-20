6b169e88deb925f5437b45a9e12ff4a6da29bd20
jeaubin <jeaubin@cisco.com>
2021-01-20 15:32:00 -0500

    20.12.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def discover_creators():
 # launch setup
 setup(
     name = 'pyats.contrib',
-    version = '20.12',
+    version = '20.12.1',
 
     # descriptions
     description = 'Open source package for pyATS framework extensions.',
